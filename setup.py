013dc3897baf34d9bd62de884025ca50142c70ef
Nikolay Chirik <nicolay.chirik@gmail.com>
2018-12-06 13:21:57 +0200

    2. Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='base_lib',
+      version='0.1',
+      description='Python data transormation utilites',
+      author='Nick',
+      author_email='wsaad@mail.ru',
+      url='http://www.python.org/sigs/distutils-sig/',
+      packages=['processing', 'helper', 'visualization'],
+)
